7fa8825d94c4690c39215f734d8c1eedb1da7d51
Dima Frolenko <mail@frolenkodima.ru>
2023-09-01 16:06:46 +0300

    fix: исправить поведение парсинга адреса на правильну функцию обновить систему получения данных регулярным выражением

--- cmd/server/urlparse.go
+++ cmd/server/urlparse.go
@@ -17,19 +17,24 @@ var validPath = regexp.MustCompile(`^/update/(?P<type>\w+)/(?P<name>\w+)/(?P<val
 
 func ParseUrl(url string) (u *URLParams, e error) {
 	groupNames := validPath.SubexpNames()
-	match := validPath.FindAllString(url, -1)
+	match := validPath.FindStringSubmatch(url)
 	if match == nil {
 		return nil, fmt.Errorf("url not matching its pattern %v", validPath)
 	}
 
 	u = &URLParams{}
-	parsedUrl := make(map[string]string)
+	//#MENTOR как бы без мапы достать из regexp красиво
 	for i, groupValue := range match {
 		groupName := groupNames[i]
-		parsedUrl[groupName] = groupValue
+		switch groupName {
+		case "type":
+			u.Type = groupValue
+		case "name":
+			u.Name = groupValue
+		case "value":
+			u.Value = groupValue
+		}
 	}
-	u.Type = parsedUrl["type"]
-	u.Name = parsedUrl["name"]
-	u.Value = parsedUrl["value"]
+
 	return
 }
