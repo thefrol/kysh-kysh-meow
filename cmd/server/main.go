9df7255de6c0d661a13880ca8e1e9c40021d9788
Dima Frolenko <mail@frolenkodima.ru>
2023-09-01 04:08:17 +0300

    Добавить базовый роутер в сервер

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,35 @@
+// Сервер Мяу-мяу
+// Умеет сохранять и передавать такие метрики
+
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func updateHandler(w http.ResponseWriter, r *http.Request) {
+	//переписать бы интерфейс так, чтобы он сразу на входе парсил строчку с командами
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusNotFound)
+		io.WriteString(w, "Мяу!")
+		return
+	}
+	io.WriteString(w, "^.^ мур!")
+	w.WriteHeader(http.StatusOK)
+}
+
+var mux *http.ServeMux
+
+func init() {
+	mux = http.NewServeMux()
+	mux.Handle("/", http.NotFoundHandler()) // отвечает на все запросы - не найдено 404
+	mux.Handle("/update/", http.HandlerFunc(updateHandler))
+}
+
+func main() {
+
+	fmt.Println("Привет, это сервер!")
+	http.ListenAndServe(":8080", mux)
+}
