a78bf7bc40a066e8772d8e674e290533153e4762
Dima Frolenko <mail@frolenkodima.ru>
2023-09-01 05:15:47 +0300

    Обернуть обработчик запроса парсингом адреса в сервере

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -9,9 +9,26 @@ import (
 	"net/http"
 )
 
-func updateHandler(w http.ResponseWriter, r *http.Request) {
-	//переписать бы интерфейс так, чтобы он сразу на входе парсил строчку с командами
+type updateHandleFunc func(http.ResponseWriter, *http.Request, URLParams)
+
+// makeHandler оборачивает функцию обработчик для маршрута
+// Проверяет, чтобы марштрут выглядел как надо и заодно парсит его и передает
+// в функцию обработчик updateHandleFunc
+func makeHandler(fn updateHandleFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		urlparams, err := ParseUrl(r.URL.Path)
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, *urlparams)
+	}
+}
+
+func updateHandler(w http.ResponseWriter, r *http.Request, params URLParams) {
+	// проверки можно отправить в makeHandler
 	if r.Method != http.MethodPost {
+		//можно использовать http.NotFound
 		w.WriteHeader(http.StatusNotFound)
 		io.WriteString(w, "Мяу! Мы поддерживаем только POST-запросы")
 		return
@@ -31,7 +48,7 @@ var mux *http.ServeMux
 func init() {
 	mux = http.NewServeMux()
 	mux.Handle("/", http.NotFoundHandler()) // отвечает на все запросы - не найдено 404
-	mux.Handle("/update/", http.HandlerFunc(updateHandler))
+	mux.Handle("/update/", makeHandler(updateHandler))
 }
 
 func main() {
