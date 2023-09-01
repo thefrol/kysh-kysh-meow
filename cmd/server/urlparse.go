ae2180634e6421da15f757f207740232dc01bee9
Dima Frolenko <mail@frolenkodima.ru>
2023-09-01 05:15:11 +0300

    Добавить парсер адреса в сервер

---
+++ cmd/server/urlparse.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+// URLParams содержит параметры, на который был разбит URL
+// тип, имя метрики, переданное значение
+type URLParams struct {
+	Type  string
+	Name  string
+	Value string
+}
+
+var validPath = regexp.MustCompile(`^/update/(?P<type>\w+)/(?P<name>\w+)/(?P<value>\w+)$`)
+
+func ParseUrl(url string) (u *URLParams, e error) {
+	groupNames := validPath.SubexpNames()
+	match := validPath.FindAllString(url, -1)
+	if match == nil {
+		return nil, fmt.Errorf("url not matching its pattern %v", validPath)
+	}
+
+	u = &URLParams{}
+	parsedUrl := make(map[string]string)
+	for i, groupValue := range match {
+		groupName := groupNames[i]
+		parsedUrl[groupName] = groupValue
+	}
+	u.Type = parsedUrl["type"]
+	u.Name = parsedUrl["name"]
+	u.Value = parsedUrl["value"]
+	return
+}
