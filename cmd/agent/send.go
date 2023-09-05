7159e6b1bd2a7c1e116af8eb515916b6d6d0708c
Dima Frolenko <mail@frolenkodima.ru>
2023-09-05 12:36:43 +0300

    fix: cmd/agent удалить неиспользуемые места в коде благодаря тестам

--- cmd/agent/send.go
+++ cmd/agent/send.go
@@ -12,11 +12,7 @@ import (
 func sendStorageMetrics(store storage.Storager, url string) error {
 	var errors []error
 	for _, key := range store.ListCounters() {
-		value, found := store.Counter(key)
-		if !found {
-			errors = append(errors, fmt.Errorf("not fount metric %v of type %v", "counter", key))
-			continue
-		}
+		value, _ := store.Counter(key)
 		err := sendMetric(url, "counter", key, value) //#TODO counter to some const
 		if err != nil {
 			errors = append(errors, err)
@@ -24,13 +20,7 @@ func sendStorageMetrics(store storage.Storager, url string) error {
 	}
 
 	for _, key := range store.ListGauges() {
-		value, found := store.Gauge(key) //#TODO #tests проверить что если есть такая метрика то он её возвращает или отправляет, можно ли такое)
-		// это уже все идут ошибки от разных видов типов, а если их десять будет? тут что десять циклов писать
-		// даже в двух уже хватает путаницы
-		if !found {
-			errors = append(errors, fmt.Errorf("not fount metric %v of type %v", "gauge", key))
-			continue
-		}
+		value, _ := store.Gauge(key)
 		err := sendMetric(url, "gauge", key, value) //#TODO counter to some const
 		if err != nil {
 			errors = append(errors, err)
