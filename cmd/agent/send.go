edef197bc31c31edb1a4460f940ab95eed00c204
Dima Frolenko <mail@frolenkodima.ru>
2023-09-05 13:33:07 +0300

    fix: cmd/agent Добавить задержку между сообщениями защита от "map concurrent write" на сервере

--- cmd/agent/send.go
+++ cmd/agent/send.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/thefrol/kysh-kysh-meow/internal/storage"
 )
@@ -40,5 +41,6 @@ func sendStorageMetrics(store storage.Storager, url string) error {
 func sendMetric(host, metric, name string, value fmt.Stringer) error {
 	url := fmt.Sprintf("%s/update/%s/%s/%s", host, metric, name, value)
 	_, err := http.Post(url, "text/plain", nil)
+	time.Sleep(200 * time.Millisecond) //защита от map concurrent write
 	return err
 }
