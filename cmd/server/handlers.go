ca95b6db89dc3595421dc1a4295657cf5b167e2c
Dima Frolenko <mail@frolenkodima.ru>
2023-09-06 05:21:22 +0300

    cmd/server переписать хендлеры под chi

--- cmd/server/handlers.go
+++ cmd/server/handlers.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/thefrol/kysh-kysh-meow/internal/metrica"
 	"github.com/thefrol/kysh-kysh-meow/internal/storage"
 )
@@ -17,23 +18,28 @@ func init() {
 	store = storage.New()
 }
 
+func UpdateHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
 // updateCounter отвечает за маршрут, по которому будет обновляться счетчик типа counter
 // иначе говоря за URL вида: /update/counter/<name>/<value>
 // приходящее значение: int64
 // поведение: складывать с предыдущим значением, если оно известно
 func updateCounter(w http.ResponseWriter, r *http.Request, params URLParams) {
-	value, err := strconv.ParseInt(params.Value(), 10, 64)
+	value, err := strconv.ParseInt(params.value, 10, 64)
 	if err != nil {
 		w.Header().Add("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "^0^ Ошибка значение, не могу пропарсить %v в %T", params.Value(), value)
+		fmt.Fprintf(w, "^0^ Ошибка значение, не могу пропарсить %v в %T", params.value, value)
+		return
 	}
-	old, _ := store.Counter(params.Name())
+	old, _ := store.Counter(params.name)
 	// по сути нам не надо обрабатывать случай, если значение небыло установлено. Оно ноль, прибавим новое значение и все четко
 	new := old + metrica.Counter(value)
-	store.SetCounter(params.Name(), new)
+	store.SetCounter(params.name, new)
 	w.Header().Add("Content-Type", "text/plain")
-	fmt.Fprintf(w, "^.^ мур! Меняем Counter %v на %v. Новое значение %v", params.Name(), value, new)
+	fmt.Fprintf(w, "^.^ мур! Меняем Counter %v на %v. Новое значение %v", params.name, value, new)
 
 	fmt.Printf("200(OK) at request to %v\n", r.URL.Path)
 }
@@ -43,15 +49,16 @@ func updateCounter(w http.ResponseWriter, r *http.Request, params URLParams) {
 // приходящее значение: float64
 // поведение: устанавливать новое значение
 func updateGauge(w http.ResponseWriter, r *http.Request, params URLParams) {
-	value, err := strconv.ParseFloat(params.Value(), 64)
+	value, err := strconv.ParseFloat(params.value, 64)
 	if err != nil {
 		w.Header().Add("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "^0^ Ошибка значение, не могу пропарсить %v в %T", params.Value(), value)
+		fmt.Fprintf(w, "^0^ Ошибка значение, не могу пропарсить %v в %T", params.value, value)
+		return
 	}
-	store.SetGauge(params.Name(), metrica.Gauge(value))
+	store.SetGauge(params.name, metrica.Gauge(value))
 	w.Header().Add("Content-Type", "text/plain")
-	fmt.Fprintf(w, "^.^ мур! меняем Gauge %v на %v.", params.Name(), value)
+	fmt.Fprintf(w, "^.^ мур! меняем Gauge %v на %v.", params.name, value)
 
 	fmt.Printf("200(OK) at request to %v\n", r.URL.Path)
 }
@@ -73,14 +80,10 @@ type updateMetricFunc func(http.ResponseWriter, *http.Request, URLParams)
 // в функцию обработчик updateHandleFunc
 func makeHandler(fn updateMetricFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// проверки можно отправить в makeHandler
-		if r.Method != http.MethodPost {
-			//можно использовать http.NotFound
-			w.Header().Add("Content-Type", "text/plain")
-			w.WriteHeader(http.StatusNotFound)
-			io.WriteString(w, "Мяу! Мы поддерживаем только POST-запросы")
-			fmt.Printf("GET request at %v\n", r.URL.Path)
-			return
+		p := URLParams{
+			metric: chi.URLParam(r, "type"),
+			name:   chi.URLParam(r, "name"),
+			value:  chi.URLParam(r, "value"),
 		}
 		// Этот блок закомментирован, чтобы пройти автотесты. В тестах отправляется запрос без text/plain и это вызывает ошибку
 		// if slices.Contains(r.Header("Content-Type"),"text/plain)" { // возможно это все щеё неправильно. Мне приходило такое "text/plain; encoging: utf8"
@@ -89,13 +92,12 @@ func makeHandler(fn updateMetricFunc) http.HandlerFunc {
 		// 	io.WriteString(w, "Мяу! Мы поддерживаем только Content-Type:text/plain")
 		// 	return
 		// }
-
-		urlparams, err := ParseURL(r.URL.Path)
-		if err != nil {
-			fmt.Printf("Cant match url %v\n", r.URL.Path)
-			http.NotFound(w, r)
-			return
-		}
-		fn(w, r, urlparams)
+		fn(w, r, p)
 	}
+}
+
+type URLParams struct {
+	metric string //type
+	name   string
+	value  string
 }
