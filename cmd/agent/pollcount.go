a965f4b687cb8c75a25a6e06f75c654881311aad
Dima Frolenko <mail@frolenkodima.ru>
2023-09-07 07:29:40 +0300

    fix: cmd/agent обновлена логика dropCounter, добавлены тесты

--- cmd/agent/pollcount.go
+++ cmd/agent/pollcount.go
@@ -6,14 +6,18 @@ import (
 )
 
 // incrementCounter обновляет PollCount счетчик в хранилище, добавляет ему единицу
-func incrementCounter(store storage.Storager, name string) error {
+func incrementCounter(store storage.Storager, name string) {
 	count, _ := store.Counter(metricPollCount)
 	store.SetCounter(metricPollCount, count+metrica.Counter(1))
-	return nil
 }
 
 // incrementCounter сбравыем счетчик
-func dropCounter(store storage.Storager, name string) error {
+func dropCounter(store storage.Storager, name string) {
+	_, found := store.Counter(metricPollCount)
+	if !found {
+		//если такого счетчика нет, то и не сбрасываем ничего
+		return
+
+	}
 	store.SetCounter(name, metrica.Counter(0))
-	return nil
 }
