f248bb6819e7157d5960b493da8b102d9c4c877a
Dima Frolenko <mail@frolenkodima.ru>
2023-09-24 06:16:00 +0300

    feat: internal/metrica добавить структуру для передачи по http

---
+++ internal/metrica/http.go
@@ -0,0 +1,14 @@
+package metrica
+
+// Metrica это универсальный тип для передачи по HTTP
+type Metrica struct {
+	ID    string   `json:"id"`              // имя метрики
+	MType string   `json:"type"`            // параметр, принимающий значение gauge или counter
+	Delta *int64   `json:"delta,omitempty"` // значение метрики в случае передачи counter
+	Value *float64 `json:"value,omitempty"` // значение метрики в случае передачи gauge
+}
+
+// Metricer используется, чтобы передать метрики по сети. Все метрики должны поддерживать этот интерфейс
+type Metrer interface {
+	Metrics(id string) Metrica
+}
