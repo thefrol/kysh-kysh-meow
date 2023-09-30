67101ccdb61fcb1ec5744dbab241bc715f60be41
Dima Frolenko <mail@frolenkodima.ru>
2023-09-30 08:49:58 +0300

    feat: storage Открыть доступ к мапам счетчиков и гаужей

--- internal/storage/memstorage.go
+++ internal/storage/memstorage.go
@@ -3,56 +3,56 @@ package storage
 import "github.com/thefrol/kysh-kysh-meow/internal/metrica"
 
 type MemStore struct {
-	counters map[string]metrica.Counter
-	gauges   map[string]metrica.Gauge
+	Counters map[string]metrica.Counter
+	Gauges   map[string]metrica.Gauge
 }
 
 func New() (m MemStore) {
-	m.counters = make(map[string]metrica.Counter)
-	m.gauges = make(map[string]metrica.Gauge)
+	m.Counters = make(map[string]metrica.Counter)
+	m.Gauges = make(map[string]metrica.Gauge)
 	return
 }
 
 // Возвращает значание именнованного счетчика, и булево значение: удалось ли счетчик найти
 func (m MemStore) Counter(name string) (metrica.Counter, bool) {
-	val, ok := m.counters[name] //Можно ли сделать в одну строчку #MENTOR
+	val, ok := m.Counters[name] //Можно ли сделать в одну строчку #MENTOR
 	return val, ok
 }
 
 func (m MemStore) SetCounter(name string, value metrica.Counter) {
-	m.counters[name] = value
+	m.Counters[name] = value
 }
 
 func (m MemStore) ListCounters() (keys []string) {
 	//keys = make([]string, 0, len(m.counters)) не такая плохая идея
-	for k := range m.counters {
+	for k := range m.Counters {
 		keys = append(keys, k)
 	}
 	return
 }
 
 func (m MemStore) Gauge(name string) (metrica.Gauge, bool) {
-	val, ok := m.gauges[name] //Можно ли сделать в одну строчку #MENTOR
+	val, ok := m.Gauges[name] //Можно ли сделать в одну строчку #MENTOR
 	return val, ok
 }
 
 func (m MemStore) SetGauge(name string, value metrica.Gauge) {
-	m.gauges[name] = value
+	m.Gauges[name] = value
 }
 
 func (m MemStore) ListGauges() (keys []string) {
 	//keys = make([]string, 0, len(m.counters)) не такая плохая идея
-	for k := range m.gauges {
+	for k := range m.Gauges {
 		keys = append(keys, k)
 	}
 	return
 }
 
 func (m MemStore) Metricas() (list []metrica.Metrica) {
-	for k, c := range m.counters {
+	for k, c := range m.Counters {
 		list = append(list, c.Metrica(k))
 	}
-	for k, g := range m.gauges {
+	for k, g := range m.Gauges {
 		list = append(list, g.Metrica(k))
 	}
 	return
