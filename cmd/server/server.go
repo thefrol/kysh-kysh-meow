7543eeff16c80bf41bce88256858e8fdc3e61b66
Dima Frolenko <mail@frolenkodima.ru>
2023-10-07 14:14:18 +0300

    feat: Создать новые классы хранилищ с сохранением + IntervalicDump сохраняет с интервалом + SyncDump - сохраняет в момент изменения метрики + переписать сервер на новые классы

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -13,17 +13,15 @@ import (
 
 	"github.com/rs/zerolog/log"
 	"github.com/thefrol/kysh-kysh-meow/internal/server/router"
+	"github.com/thefrol/kysh-kysh-meow/internal/storage"
 )
 
 func main() {
 	cfg := mustConfigure(defaultConfig)
 
 	// создаем хранилище
-	s, err := fileStorage(cfg)
-	if err != nil {
-		log.Error().Msgf("Не удалось сконфигурировать сервер, по причине: %v", err)
-		return
-	}
+	m := storage.New()
+	s, cancelStorage := ConfigureStorage(&m, cfg)
 
 	// Запускаем сервер с поддержкой нежного выключения
 	// вдохноввлено примерами роутера chi
@@ -66,24 +64,59 @@ func main() {
 
 	// Завершаем последние дела
 	// попытаемся сохраниться в файл
+	cancelStorage()
 
-	// saver позволяет получить доступ к функции сохранения в хранилище,
-	// потому что в Storager мы его не имеем, и не очень-то хотим
-	if cfg.FileStoragePath != "" { //todo можно сделать как функцию в структуре config
-		type saver interface {
-			ToFile(string) error
-		}
-
-		if v, ok := s.(saver); ok {
-			v.ToFile(cfg.FileStoragePath)
-			log.Info().Msg("Сохранено в файл")
-		} else {
-			log.Error().Msg("Не могу преобразовать хранилище в нужный интерфейс для сохранения данных на выходе")
-		}
-	}
+	time.Sleep(time.Second)
 
 	log.Info().Msg("^.^ Сервер завершен нежно")
 	// Wait for server context to be stopped
 	<-serverCtx.Done()
 
+}
+
+func ConfigureStorage(m *storage.MemStore, cfg config) (storage.Storager, context.CancelFunc) {
+	// 1. Если путь не задан, то возвращаем хранилище в оперативке, без приблуд
+	// 2. Иначе оборачиваем файловым хранилищем, но не возвращаем пока
+	// 3. Если Restore=true, то читаем из файла. Если файла не существует, то игнорируем проблему
+	// 4. Оборачиваем файловое хранилище сихнронным или интервальным сохранением
+
+	// Если путь до хранилища не пустой, то нам нужно инициаизировать обертки над хранилищем
+	if cfg.FileStoragePath == "" {
+		return m, func() {
+			log.Info().Msg("Хранилище сихронной записи получило сигнал о завершении, но файловая запись в текущей конфигурации сервера не используется. Ничего не записано")
+		}
+	}
+
+	// Оборачиваем файловым хранилищем, в случае, есл и
+	fs := storage.NewFileStorage(m, cfg.FileStoragePath)
+	if cfg.Restore {
+		err := fs.Restore()
+		// в случае, если файла не существует, игнорируем эту проблему
+		if err != nil && err != storage.ErrorRestoreFileNotExist {
+			panic(err)
+		}
+	}
+
+	if cfg.StoreIntervalSeconds == 0 {
+		// инициализируем сихнронную запись,
+		// при этом сохраняться в конце нам не понадобится
+		return storage.NewSyncDump(&fs, cfg.FileStoragePath), func() {
+			log.Info().Msg("Хранилище сихронной записи получило сигнал о завершении, но дополнительно сохранение не нужно")
+		}
+	}
+
+	// Запускаем интервальную запись и создаем токен отмены, при необходимости сюда можно будет добавить и группу ожидания
+	s := storage.NewIntervalDump(&fs, time.Duration(cfg.StoreIntervalSeconds)*time.Second)
+	ctx, cancel := context.WithCancel(context.Background())
+	go s.StartDumping(ctx)
+
+	return s, func() {
+		// оберстка сделана под группу ожидаения
+		cancel()
+	}
+
+	// TODO
+	//
+	// Пока что судя по всему эта функция эвакуируем моё хранилище из стека, мне кажется, но не мапы
+	// Как вариант сразу создавать FileStorage, и просто не оборачивать его если надо
 }
---
+++ internal/storage/storagedumper.go
@@ -0,0 +1,124 @@
+package storage
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/rs/zerolog/log"
+	"github.com/thefrol/kysh-kysh-meow/internal/metrica"
+)
+
+var ErrorRestoreFileNotExist = fmt.Errorf("файла, чтобы выгрузить хранилише не существует")
+
+// FileStorage позволяет писать и восстанавливаться из файда
+// при помощи функций Dump() и Restore(). Является оберткой над
+// типом memStore
+type FileStorage struct {
+	MemStore
+	FileName string
+}
+
+func NewFileStorage(m *MemStore, fileName string) FileStorage {
+	return FileStorage{MemStore: *m, FileName: fileName}
+}
+
+// Перезаписать данные значением из файла fname
+func (s FileStorage) Restore() error {
+	if !fileExist(s.FileName) {
+		return ErrorRestoreFileNotExist
+	}
+	err := RewriteFromFile(s.FileName, &s.MemStore)
+	if err != nil {
+		return fmt.Errorf("ошибка чтения файла %v при загрузке хранилища: %v", s.FileName, err)
+	}
+	return nil
+	// todo
+	//
+	// прям сюда весь код из MemStore по сохранению и загрузке
+}
+
+func fileExist(file string) bool {
+	if s, err := os.Stat(file); err == nil && !s.IsDir() {
+		return true
+	} else if errors.Is(err, os.ErrNotExist) {
+		return false
+
+	} else {
+		// Schrodinger: file may or may not exist. See err for details.
+
+		// Therefore, do *NOT* use !os.IsNotExist(err) to test for file existence
+		return false
+	}
+}
+
+func (s FileStorage) Dump() error {
+	return s.MemStore.ToFile(s.FileName)
+}
+
+type IntervalDump struct {
+	FileStorage
+	Interval time.Duration
+}
+
+func NewIntervalDump(s *FileStorage, interval time.Duration) IntervalDump {
+	return IntervalDump{FileStorage: *s, Interval: interval}
+	// TODO
+	//
+	// Стоит ли MemStore принимать в аргументе, поможет ли это держать все в стеке?
+}
+
+func (s IntervalDump) StartDumping(ctx context.Context) {
+	t := time.NewTicker(s.Interval)
+	defer t.Stop()
+	// todo а как закрывать канал?
+
+	for {
+		select {
+		case <-t.C:
+			// когда тикает таймер, мы просто записываем данные в хранилище. Если не получилось - ничего страшного
+			err := s.Dump()
+			if err != nil {
+				log.Error().Msgf("Не удалось записать хранилише в файл %v: %+v", s.FileName, err)
+				continue
+			}
+			log.Info().Msgf("Хранилище записано в %v", s.FileName)
+		case <-ctx.Done():
+			// Если пришел запрос на отключение - тоже записываем в файл
+			err := s.Dump()
+			if err != nil {
+				log.Error().Msgf("При завершении работы IntervalDumper, не удалось записать хранилише в файл %v: %+v", s.FileName, err)
+			}
+			log.Info().Msgf("Запись по интервалу прекращает свою работу")
+			return
+		}
+	}
+}
+
+type SyncDump struct {
+	FileStorage
+}
+
+func NewSyncDump(s *FileStorage, fileName string) SyncDump {
+	return SyncDump{FileStorage: *s}
+}
+
+func (s SyncDump) SetCounter(name string, value metrica.Counter) {
+	s.FileStorage.SetCounter(name, value)
+	err := s.Dump()
+	if err != nil {
+		log.Error().Msgf("Не удалось записать в хранилише %v: %v", s.FileName, err)
+	}
+	log.Info().Msgf("После обновления метрики %v[Counter], хранилище записино в %v", name, s.FileName)
+
+}
+func (s SyncDump) SetGauge(name string, value metrica.Gauge) {
+	s.FileStorage.SetGauge(name, value)
+	err := s.Dump()
+	if err != nil {
+		log.Error().Msgf("Не удалось записать в хранилише %v: %v", s.FileName, err)
+	}
+	log.Info().Msgf("После обновления метрики %v[Gauge], хранилище записино в %v", name, s.FileName)
+}
