8301dc738572bf478e29bb0ed3fb960eb33d81d0
Dima Frolenko <mail@frolenkodima.ru>
2023-10-12 10:55:55 +0300

    fix: api Валидировать структуры самому

--- internal/server/api/v2/json_handlers.go
+++ internal/server/api/v2/json_handlers.go
@@ -5,12 +5,17 @@ package apiv2
 
 import (
 	"context"
+	"errors"
 	"net/http"
 
 	"github.com/mailru/easyjson"
-	"github.com/rs/zerolog/log"
 	"github.com/thefrol/kysh-kysh-meow/internal/metrica"
 	"github.com/thefrol/kysh-kysh-meow/internal/server/api"
+)
+
+var (
+	ErrorDeltaEmpty = errors.New("Поле Delta не может быть пустым, для когда id=counter")
+	ErrorValueEmpty = errors.New("Поле Value не может быть пустым, для когда id=gauge")
 )
 
 // API это колленция http.HanlderFunc, которые обращаются к единому хранилищу store
@@ -26,129 +31,84 @@ func New(store api.Storager) API {
 	return API{store: store}
 }
 
-// Не знаю, правильно ли это обновлять значение m в функциях AddValueFromStorage и UpdateStorageAndValue.
-// Просто не хочется ещё раз обращаться к хранилищу, там проводить преобразование типов  и плодить указатели.
-// Тут же у нас указатели в полях m.Delta и m.Value по заданию. Надо бы, конечно, провести эскейп анализ этого кода
-//
-// С точки зрения скорости такой код хорош, но кажется он не надежен и не прозрачен
-//
-// По иронии, с такими функциями код тепеь не дублируется, потому что при update не надо вызывать get из хранилиша.
-// Но мне нравится, как они разгружают внешний вид самых хендлеров
-//
-// Есть ощущение, что можно объекдинить get и update в одну вспомогательную функцию. Это вроде как уберет дублирование кода, но
-// лишь виртуально. Мы точно не знаем, добавятся не разойдутся ли эти функции в будущем. И вообще, мы нарушим принцип единственной ответственности.
-// Кроме того, там добавится куча проверок; получим код, который я уверен, через неделю или две,  даже я не смогу понять, как работает.
-// Эти проверки будут тормозить код. Так что с точки зрения и производительности и читаемости, мы все же разобьем на две функции, пусть они очень похожи,
-// и изначальная задача была все же избавиться от дублирования.
-//
-// Нужно иметь в виду, что обработка этих хендлеров - особо чувствительная область. Представим, что мы пишем не в оперативную память,
-// а в базу данных. Дополнительно вазывать чтение значение из базы - записали нормально ли? Не слишком ли это много времени займет, так что
-// эти два хендлера работают в рачете на скорость, потому что тут это наиболее чувствительная область.
+func MarshallUnmarshallMerica(handler func(context.Context, metrica.Metrica) (out metrica.Metrica, err error)) func(http.ResponseWriter, *http.Request) {
 
-//
+	return func(w http.ResponseWriter, r *http.Request) {
+		/*
+			1. Размаршаливаем полученное сообщение в структуру metrica.Metrica
+			2. Валидиуем полученную структуру
+			3. Запускаем обработчик handler(), он как-то там работает с хранилищем, или ещё чего, что нам не очень важно.
+				И возвращает структуру out.
+			4. Замаршаливаем результат работы хендлера
+		*/
 
-// updateWithJSON обновляет значение счетчика JSON запросом. Читает из запроса тело в
-// формате metrica.Metrica. Для счетчиков типа counter исползует поле delta и прибавляет к
-// текущему значению, для счетчиков типа gauge заменяет текущее значение новым из поля Value.
-// В ответ записывает структуру metrica.Metrica с обновленным значением
-func (i API) UpdateWithJSON(w http.ResponseWriter, r *http.Request) {
-	//todo mix of two handlers + save+ response
-	m := metrica.Metrica{}
-	err := easyjson.UnmarshalFromReader(r.Body, &m)
-	if err != nil {
-		log.Error().Str("location", "json update handler").Msg("Cant unmarshal data in body")
-		http.Error(w, "^0^ не могу размаршалить тело сообщения", http.StatusBadRequest)
-		return
-	}
-	defer r.Body.Close()
-
-	// проверяем полученную структуру
-	if err = m.Validate(); err != nil {
-		api.HTTPErrorWithLogging(w, http.StatusBadRequest, "Получена неправильно заполенная струкура %+v: %v", m, err)
-		return
-	}
-	if err = m.ContainsUpdate(); err != nil {
-		api.HTTPErrorWithLogging(w, http.StatusBadRequest, "Получена неправильно заполенная струкура %+v: %v", m, err)
-		return
-	}
-
-	// обновляем
-	val, err := updateStorage(r.Context(), i.store, m)
-	if err != nil {
-		api.HTTPErrorWithLogging(w, http.StatusBadRequest, "Ошибка обновления хранилища: %v", err)
-		return
-	}
-
-	_, _, err = easyjson.MarshalToHTTPResponseWriter(&val, w)
-	if err != nil {
-		log.Error().Str("location", "json update handler(on return)").Msgf("Cant marshal a return for %v %v", m.MType, m.ID)
-		http.Error(w, "cant marshal result", http.StatusInternalServerError)
-		return
-	}
-}
-
-// value WithJSON возвращает значение счетчика JSON запросом. Читает из запроса тело в
-// формате metrica.Metrica, у которого должны быть заполенены поля MType и ID,
-// TДля счетчиков типа counter записывает значнием в поле delta,
-// для счетчиков типа gauge в поле value. В ответ
-// записывает структуру metrica.Metrica с обновленным значением
-func (i API) ValueWithJSON(w http.ResponseWriter, r *http.Request) {
-	//todo mix of two handlers + save+ response
-	m := metrica.Metrica{}
-	err := easyjson.UnmarshalFromReader(r.Body, &m)
-	if err != nil {
-		log.Error().Str("location", "json update handler").Msg("Cant unmarshal data in body")
-		http.Error(w, "bad body", http.StatusBadRequest)
-		return
-	}
-	defer r.Body.Close()
-
-	// проверяем полученную структуру
-	if err = m.Validate(); err != nil {
-		api.HTTPErrorWithLogging(w, http.StatusBadRequest, "Получена неправильно заполенная струкура %+v: %v", m, err)
-		return
-	}
-
-	val, err := GetStorage(r.Context(), i.store, m)
-	if err != nil {
-		if err == api.ErrorNotFoundMetric {
-			api.HTTPErrorWithLogging(w, http.StatusNotFound, "В хранилище не найдена метрика %v", m.ID)
+		// Размаршаливаем полученное сообщение в структуру metrica.Metrica
+		in := metrica.Metrica{}
+		err := easyjson.UnmarshalFromReader(r.Body, &in)
+		if err != nil {
+			api.HTTPErrorWithLogging(w, http.StatusBadRequest, "Не могу размаршалить тело сообщения: %v", err)
 			return
 		}
-		api.HTTPErrorWithLogging(w, http.StatusBadRequest, "Ошибка обновления метрики %+v: %v", m, err) // todo, а как бы сделать так, чтобы %v подсвечивался
-		return
+		defer r.Body.Close()
+
+		// Валидиуем полученную структуру
+		if in.ID == "" {
+			api.HTTPErrorWithLogging(w, http.StatusBadRequest, "Получена направильно заполенная струкура %+v: имя метрики не может быть пустым", in)
+			return
+		}
+
+		// Запускаем обработчик handler()
+		out, err := handler(r.Context(), in)
+		if err != nil {
+			if err == api.ErrorNotFoundMetric {
+				api.HTTPErrorWithLogging(w, http.StatusNotFound, "В хранилище не найдена метрика %v", in.ID)
+				return
+			}
+			api.HTTPErrorWithLogging(w, http.StatusBadRequest, "Ошибка обновления метрики %+v: %v", in, err) // todo, а как бы сделать так, чтобы %v подсвечивался
+			return
+		}
+
+		// Замаршаливаем результат работы хендлера
+		_, _, err = easyjson.MarshalToHTTPResponseWriter(&out, w)
+		if err != nil {
+			api.HTTPErrorWithLogging(w, http.StatusInternalServerError, "Не могу замаршалить выходной джейсон: %v", err)
+			return
+		}
 	}
 
-	_, _, err = easyjson.MarshalToHTTPResponseWriter(&val, w)
-	if err != nil {
-		log.Error().Str("location", "json get handler(on return)").Msgf("Cant marshal a return for %v %v", m.MType, m.ID)
-		http.Error(w, "cant marshal result", http.StatusInternalServerError)
-		return
-	}
 }
 
-func updateStorage(ctx context.Context, store api.Storager, upd metrica.Metrica) (newVal metrica.Metrica, err error) {
-	switch upd.MType {
+func (a API) UpdateStorage(ctx context.Context, in metrica.Metrica) (newVal metrica.Metrica, err error) {
+
+	switch in.MType {
 	case "counter":
-		c, err := store.IncrementCounter(ctx, upd.ID, *upd.Delta)
-		return metrica.Metrica{MType: upd.MType, ID: upd.ID, Delta: &c}, err // это получается отправится в хип
+		if in.Delta == nil {
+			return empty, ErrorDeltaEmpty
+		}
+		c, err := a.store.IncrementCounter(ctx, in.ID, *in.Delta)
+		return metrica.Metrica{MType: in.MType, ID: in.ID, Delta: &c}, err // это получается отправится в хип
 	case "gauge":
-		g, err := store.UpdateGauge(ctx, upd.ID, *upd.Value)
-		return metrica.Metrica{MType: upd.MType, ID: upd.ID, Value: &g}, err
+		if in.Value == nil {
+			return empty, ErrorValueEmpty
+		}
+		g, err := a.store.UpdateGauge(ctx, in.ID, *in.Value)
+		return metrica.Metrica{MType: in.MType, ID: in.ID, Value: &g}, err
 	default:
-		return
+		return empty, api.ErrorUnknownMetricType
 	}
 }
 
-func GetStorage(ctx context.Context, store api.Storager, req metrica.Metrica) (newVal metrica.Metrica, err error) {
-	switch req.MType {
+func (a API) GetStorage(ctx context.Context, in metrica.Metrica) (newVal metrica.Metrica, err error) {
+	switch in.MType {
 	case "counter":
-		c, err := store.Counter(ctx, req.ID)
-		return metrica.Metrica{MType: req.MType, ID: req.ID, Delta: &c}, err // это получается отправится в хип
+		c, err := a.store.Counter(ctx, in.ID)
+		return metrica.Metrica{MType: in.MType, ID: in.ID, Delta: &c}, err // это получается отправится в хип
 	case "gauge":
-		g, err := store.Gauge(ctx, req.ID)
-		return metrica.Metrica{MType: req.MType, ID: req.ID, Value: &g}, err
+		g, err := a.store.Gauge(ctx, in.ID)
+		return metrica.Metrica{MType: in.MType, ID: in.ID, Value: &g}, err
 	default:
-		return
+		return empty, api.ErrorUnknownMetricType
 	}
 }
+
+var empty = metrica.Metrica{}
