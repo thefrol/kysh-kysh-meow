3cfce8e7d143e51434c9e618cf27a1eac5b1dc2a
Dima Frolenko <mail@frolenkodima.ru>
2023-10-13 11:53:38 +0300

    fix: apiv3 исправить передаваемый контекст

--- internal/server/api/v3/db_state.go
+++ internal/server/api/v3/db_state.go
@@ -1,15 +1,11 @@
 package apiv3
 
 import (
-	"context"
 	"net/http"
-	"time"
 
 	"github.com/thefrol/kysh-kysh-meow/internal/server/api"
 	"github.com/thefrol/kysh-kysh-meow/internal/server/app"
 )
-
-const waitTime = time.Second * 1
 
 type API struct {
 	app *app.App
@@ -20,9 +16,7 @@ func New(app *app.App) API {
 }
 
 func (i API) CheckConnection(w http.ResponseWriter, r *http.Request) {
-	ctx, cancel := context.WithTimeout(i.app.Context(), waitTime)
-	defer cancel()
-	err := i.app.CheckConnection(ctx)
+	err := i.app.CheckConnection(r.Context())
 	if err != nil {
 		api.HTTPErrorWithLogging(w, http.StatusInternalServerError, "^0^ Соединение с базой данных отсуствует")
 	}
