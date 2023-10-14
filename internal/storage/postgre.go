d1ff468792f8218f5e4aaf4e42cf2ab283b636e7
Dima Frolenko <mail@frolenkodima.ru>
2023-10-14 10:49:26 +0300

    feat: Storage воплотить интерфейс Operator в Database

--- internal/storage/postgre.go
+++ internal/storage/postgre.go
@@ -5,6 +5,8 @@ import (
 	"database/sql"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/thefrol/kysh-kysh-meow/internal/metrica"
+	"github.com/thefrol/kysh-kysh-meow/internal/server/api"
 )
 
 // Database это приложение сервера, которое умеет работать с базой данных, и другими хранилищами. Со всеми вещами от которого, он зависит.
@@ -28,9 +30,24 @@ func NewPostGresDatabase(ctx context.Context, connString string) (*Database, err
 	}, nil
 }
 
-// CheckConnection проверяет соединение с базой данных, в случае ошибки возвращает error!=nil
-func (d Database) CheckConnection(ctx context.Context) error {
+// Check implements api.Operator. проверяет соединение с базой данных, в случае ошибки возвращает error!=nil
+func (d *Database) Check(ctx context.Context) error {
 	return d.db.PingContext(ctx)
+}
+
+// Get implements api.Operator.
+func (*Database) Get(ctx context.Context, req ...metrica.Metrica) (resp []metrica.Metrica, err error) {
+	panic("unimplemented")
+}
+
+// List implements api.Operator.
+func (*Database) List(ctx context.Context) (counterNames []string, gaugeNames []string, err error) {
+	panic("unimplemented")
+}
+
+// Update implements api.Operator.
+func (*Database) Update(ctx context.Context, req ...metrica.Metrica) (resp []metrica.Metrica, err error) {
+	panic("unimplemented")
 }
 
 func (d Database) Database() *sql.DB {
@@ -41,3 +58,5 @@ func (d Database) Context() context.Context {
 	// todo не понимаю можно ли так передавать контекст по значению
 	return d.ctx
 }
+
+var _ api.Operator = (*Database)(nil)
