93e07040c91406a366db74685476785eb72e183e
Dima Frolenko <mail@frolenkodima.ru>
2023-10-17 17:47:56 +0300

    feat: lib добавлен пакет retry для повторного запуска функций

---
+++ lib/retry/do.go
@@ -0,0 +1,75 @@
+package retry
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+type Options struct {
+	delays     []time.Duration
+	maxretries int
+	conditions []func(error) bool
+	callbacks  []func(n int)
+}
+
+// This запускает несколько попыток запустить функцию,
+// новые попытки будут совершаться, только ошибка
+// обернута при помощи retry.Retriable(), или если выполняется
+// одно из условий переданных при помощи If() или IfError()
+func This(f func() error, opts ...Option) error {
+
+	options := Options{
+		maxretries: 1,
+	}
+
+	for _, opt := range opts {
+		err := opt(&options)
+		if err != nil {
+			return fmt.Errorf("ошибка опции при запуске восстанавливаемой функции: %w", err)
+		}
+	}
+
+	var err error
+	for i := 0; i < options.maxretries; i++ {
+		// Мы пускаем массив задержек по кругу, кроме первой попытки
+		if i > 0 {
+			currentI := int((i - 1) % len(options.delays))
+			time.Sleep(options.delays[currentI])
+		}
+
+		err = f()
+		if err == nil {
+			return nil
+		}
+		if canretry(err, options) {
+			for _, c := range options.callbacks {
+				c(i)
+			}
+			continue
+		}
+
+		return err
+	}
+	return err
+}
+
+func canretry(err error, opts Options) bool {
+	// todo
+	//
+	// Интересная тема, что в самой бы ошибке
+	// можно было бы указать сколько раз функцию можно
+	// перезапустить и с какими интервалами
+	var re *RetriableError
+	if errors.As(err, &re) {
+		// запускаем коллбеки перед повторным запуском
+		return true
+	}
+
+	for _, cond := range opts.conditions {
+		if cond(err) {
+			return true
+		}
+	}
+	return false
+}
---
+++ lib/retry/optfuncs.go
@@ -0,0 +1,59 @@
+package retry
+
+import (
+	"errors"
+	"time"
+)
+
+type Option func(opt *Options) error
+
+func DelaySeconds(seconds ...uint) Option {
+	return func(opt *Options) error {
+		var delays []time.Duration
+		for _, interval := range seconds {
+			delays = append(delays, time.Second*time.Duration(interval))
+		}
+		opt.delays = delays
+		return nil
+	}
+}
+
+func OnRetry(funs ...func(int)) Option {
+	return func(opt *Options) error {
+		opt.callbacks = append(opt.callbacks, funs...)
+		return nil
+	}
+}
+
+func Attempts(count uint) Option {
+	return func(opt *Options) error {
+		opt.maxretries = int(count)
+		return nil
+	}
+}
+
+// If позволяет указать функцию, которая проверит можно ли с
+// ошибкой err делать ещё одну попытку.
+//
+// В одном запуске может быть несколько таких условий, функция сработает
+// если выполнится одно из них
+func If(f func(error) bool) Option {
+	return func(opt *Options) error {
+		opt.conditions = append(opt.conditions, f)
+		return nil
+	}
+}
+
+// IfError позволяет повторить запуск, если повторяемая функция вернула
+// определенную ошибку, например io.EOF
+//
+// В одном запуске может быть несколько таких условий, функция сработает
+// если выполнится одно из них
+func IfError(target error) Option {
+	return func(opt *Options) error {
+		opt.conditions = append(opt.conditions, func(err error) bool {
+			return errors.Is(err, target)
+		})
+		return nil
+	}
+}
