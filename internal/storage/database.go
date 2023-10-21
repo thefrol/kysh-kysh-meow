cdc6f30196bce1fd2ccfc4b4b78c1ee0b7bf19ed
Dima Frolenko <mail@frolenkodima.ru>
2023-10-21 11:11:24 +0300

    fix: storage Упростить запрос postgres для обновления Counter

--- internal/storage/database.go
+++ internal/storage/database.go
@@ -25,8 +25,7 @@ const (
 
 	queryList = "SELECT 'counter',id FROM counters UNION SELECT 'gauge',id from gauges;"
 
-	queryUpdateCounter = "UPDATE counters SET delta=delta+$2 WHERE id=$1"
-	queryInsertCounter = "INSERT INTO counters VALUES ($1,$2);"
+	queryUpsertCounter = "INSERT INTO counters VALUES ($1,$2) ON CONFLICT (id) DO UPDATE SET delta=counters.delta+$2;"
 	queryUpsertGauge   = "INSERT INTO gauges VALUES ($1,$2) ON CONFLICT (id) DO UPDATE SET value=$2;"
 )
 
@@ -147,33 +146,13 @@ func (d *Database) Update(ctx context.Context, req ...metrica.Metrica) (resp []m
 	for _, r := range req {
 		switch r.MType {
 		case "counter":
-			/*
-				1. Пробуем делать UPDATE ... SET value=value+delta
-				2. Если количество обновленных полей =0, делаем INSERT INTO
-			*/
-
 			if r.Delta == nil {
 				return nil, api.ErrorDeltaEmpty
 			}
 
-			// todo не помню, надо ли тут проверять на всякие пустые ссылки...
-			rs, err := tx.ExecContext(ctx, queryUpdateCounter, r.ID, r.Delta)
+			_, err := tx.ExecContext(ctx, queryUpsertCounter, r.ID, r.Delta)
 			if err != nil {
 				return nil, err
-			}
-			count, err := rs.RowsAffected()
-			if err != nil {
-				return nil, err
-			}
-			if count > 1 {
-				log.Warn().Msgf("При транзакции обновлено несколько строк %+v", r)
-			}
-			if count == 0 {
-				// Значит счетчик не создан, значит создадим
-				_, err := tx.ExecContext(ctx, queryInsertCounter, r.ID, r.Delta)
-				if err != nil {
-					return nil, err
-				}
 			}
 
 		case "gauge":
