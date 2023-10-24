1aa6d6fbaa93de35edb197306635922fe27a087e
Dima Frolenko <mail@frolenkodima.ru>
2023-10-24 14:11:31 +0300

    feat: middleware убрать старый врайтер-обертку

--- internal/server/middleware/log.go
+++ internal/server/middleware/log.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"fmt"
 	"net/http"
 	"time"
 
@@ -35,7 +34,6 @@ func MeowLogging() func(http.Handler) http.Handler {
 				// запустить обработку
 				next.ServeHTTP(&wr, r)
 			})
-			fmt.Println(wr.StatusCode())
 
 			// TODO
 			// Возможно в одном ответе мы можем передавать просто две структуры! и в сообщении какие-то самые важные моменты
@@ -65,13 +63,6 @@ func MeowLogging() func(http.Handler) http.Handler {
 	}
 }
 
-// wrapWriter Оборачивает оригинальный http.ResponseWriter оболочкой writeWrapper, которая хранит
-// некоторые данные о манипуляциях с этим врайтером. Например, какой статус код был записан,
-// сколько данных записано в байтах
-func wrapWriter(w http.ResponseWriter) *writerWrapper {
-	return &writerWrapper{originalWriter: w, statusCode: 200}
-}
-
 // countTime засекает время исполнения функции аргумента
 func countTime(f func()) (d time.Duration) {
 	defer func() {
@@ -81,54 +72,3 @@ func countTime(f func()) (d time.Duration) {
 	return
 	// хрена се, эта функция работает примерно в тысячу раз быстрее чем прошлая с указателем
 }
-
-// writeWrapper это оболочка для интерфейса http.ResponseWriter, которая хранит некоторые параметры о
-// манипуляциях с ней: сколько было записино, и какие данные под конец, так же отслеживает чтобы итоговый статус код был правильно записан
-type writerWrapper struct {
-	originalWriter http.ResponseWriter
-	bytesWritten   int
-	statusCode     int
-} //  todo этот класс так или иначе используется в каждой мидлвари
-
-func (ww *writerWrapper) Header() http.Header {
-	return ww.originalWriter.Header()
-}
-func (ww *writerWrapper) Write(bb []byte) (int, error) {
-	n, err := ww.originalWriter.Write(bb)
-	ww.bytesWritten += n
-	return n, err
-}
-
-func (ww *writerWrapper) WriteHeader(statusCode int) {
-	if ww.bytesWritten > 0 {
-		// я кое-что узнал в перерыве, что после использования Write()
-		// заголовки нельзя больше переписать даже при помощи WriteHeader()
-		// поэтому проверяем
-		log.Error().
-			Str("location", "logger middleware").
-			Msg("Попытка записи заголовков после использования функции Write(). Заголовки и статус уже не изменить")
-
-		// TODO
-		//
-		// На данный момент, при вызове http.Error(...),
-		// он успевает как-то и записать в тело, и только
-		// потом потом делает WriteHeader(), что по моим
-		// наблюдениям попросту невозможно. Из-за этого
-		// Появляются лишние сообщения об эшибках. В любом
-		// случае этим не логгер должен заниматься
-	}
-
-	if statusCode == 0 {
-		log.Error().Str("location", "logger middleware").Msg("Кто-то пытается записать в ответ статус код 0, это ошибка и приведет к падению сервера")
-	}
-	if statusCode != 200 {
-		ww.originalWriter.WriteHeader(statusCode)
-	}
-	ww.statusCode = statusCode
-
-}
-
-// проверить что writeWrapper отвечает интерфейсу http.ResponseWriter
-var _ http.ResponseWriter = (*writerWrapper)(nil)
-
-// тут по-хорошему сделать бы инъекцию зависимости и предоставить нужный журнал, но нормального интерфейса у Олологгера у нас нет
