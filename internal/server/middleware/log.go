918cc0b23052f465772b49393b02f1422b9b5881
Dima Frolenko <mail@frolenkodima.ru>
2023-10-25 12:17:19 +0300

    fix: middleware Освежить журналирующую мидлварь

--- internal/server/middleware/log.go
+++ internal/server/middleware/log.go
@@ -5,58 +5,31 @@ import (
 	"time"
 
 	"github.com/rs/zerolog/log"
+	"github.com/thefrol/kysh-kysh-meow/internal/sign"
 	"github.com/thefrol/kysh-kysh-meow/lib/intercept"
 )
 
 func MeowLogging() func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-			d := time.Duration(0)
-			wr := intercept.New(w, make([]byte, 0, 1024))
-			//defer wr.Close() // очень показательная ошибка!
-			// тут мы в дефер передали wr со всеми старыми значениями
-			// wr.StatusCode и всяких полей.
-			// а по логике моей структурки, я записываю этот StatusCode
-			// в ответ, приложение потом запишет туда 500
-			// но в close() будет звучать все равно исходный 0,
-			// который был там записан во время вызова defer
-			//
-			// тут конечно очень большое поле для выбора как это фиксить
-			// можно передавать в дефер замыкание, можно вызывать клоуз
-			// по ссылки и тогда передавать по ссылке объект в дефер
-			//
-			// я выбрал вариант где Close() имеет ресивер по указателю,
-			// собсно в этом вся беда и была
-			defer wr.Close()
-
-			d = countTime(func() {
+			faker := intercept.WithBytesCounter(w)
+			d := countTime(func() {
 				// запустить обработку
-				next.ServeHTTP(&wr, r)
+				next.ServeHTTP(faker, r)
 			})
-
-			// TODO
-			// Возможно в одном ответе мы можем передавать просто две структуры! и в сообщении какие-то самые важные моменты
-			//
-			// Вообще мне не очень нравится такой формат, как в задании
-			// хотелось бы видеть - пришел запрос номер такой-то
-			// запрос такой-то
-			// тут сообщения от хендера посередине
-			// ответ такой-то
-			// Чтобы сообщения были как бы обернуты миддлеваром, вот начало вот конец.
-			// Или в контексте реквеста передается как-то логгер, куда может писать хендлер и тогда его сообщения
-			// будут как-то отдельно форматироваться
 			log.Info().
 				Str("method", r.Method).
 				Str("uri", r.RequestURI).
 				Bool("gzippedRequest", encoded(r, "gzip")).
-				Dur("duration", d).
+				Str("Sign", r.Header.Get(sign.SignHeaderName)). // todo sign.HeaderName
+				Dur("Duration", d).
 				Msg("Request ->")
 
 			log.Info().
-				Int("statusCode", wr.StatusCode()).
-				Str("Content-Type", wr.Header().Get("Content-Type")).
-				Int("size", wr.Buf().Len()).
+				Int("statusCode", faker.StatusCode()).
+				Str("Content-Type", w.Header().Get("Content-Type")).
+				Str("Sign", w.Header().Get(sign.SignHeaderName)).
+				Int("Size", faker.BytesWritten()).
 				// todo add gzipped response flag
 				Msg("Response ->")
 		})
