1b2148151b56475d1348ae99afc0447f9b320a17
Dima Frolenko <mail@frolenkodima.ru>
2023-10-26 14:29:04 +0300

    fix: Создавать хранилище в методе config.Server

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,13 +1,18 @@
 package config
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/caarlos0/env/v6"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"github.com/thefrol/kysh-kysh-meow/internal/server/api"
+	"github.com/thefrol/kysh-kysh-meow/internal/storage"
 )
 
 type Server struct {
@@ -47,6 +52,105 @@ func (cfg *Server) Parse(defaults Server) error {
 		cfg.FileStoragePath = v
 	}
 	return nil
+}
+
+// ConfigureStorage подготавливает хранилище к работе в соответствии с текущими настройками,
+// при необходимости загружает из файла значения метрик, запускает сохранение в файл, и
+// возвращает интерфейс хранилища и функцию, подготавливающая ханилище к остановке
+//
+// На входе получает экземпляр хранилища m, и далее оборачивает его другим классов,
+// наиболее соответсвующим задаче, исходя из cfg
+func (cfg Server) MakeStorage() (api.Operator, context.CancelFunc) {
+	// 0. Если указана база данных, создаем хранилище с базой данных
+	// 1. Если путь не задан, то возвращаем хранилище в оперативке, без приблуд
+	// 2. Иначе оборачиваем файловым хранилищем, но не возвращаем пока
+	// 3. Если Restore=true, то читаем из файла. Если файла не существует, то игнорируем проблему
+	// 4. Оборачиваем файловое хранилище сихнронным или интервальным
+
+	// TODO
+	//
+	// Думаю эта функция должна получать на вход контекст, а не возвращать CancelFunc
+	//
+	// И нужно убрать отсюда все паники
+
+	// Если база данных
+	if cfg.DatabaseDSN != "" {
+		db, err := sql.Open("pgx", cfg.DatabaseDSN)
+		if err != nil {
+			e := fmt.Errorf("не могу создать соединение с БД: %w", err)
+			panic(e)
+		}
+
+		dbs, err := storage.NewDatabase(db)
+		if err != nil {
+			log.Error().Msgf("Ошибка создания хранилища в базе данных - %v", err)
+			panic(err)
+		}
+
+		if err := dbs.Check(context.TODO()); err != nil {
+			log.Error().Msgf("Нет соединения с БД - %v", err)
+		}
+
+		log.Info().Msg("Создано хранилише в Базе данных")
+		return dbs, func() {
+			err := db.Close()
+			if err != nil {
+				log.Error().Msgf("Не могу закрыть базу данных: %v", err)
+			}
+
+			// todo
+			//
+			// Конечно, я хочу делать это defer или как-то так, можно у нас будет некий app.Close()
+		}
+	}
+
+	// Если не база данных, то начинаем с начала - создаем хранилище в памяти, и оборачиваем его всякими штучками если надо
+	m := storage.New()
+
+	// Если путь до хранилища не пустой, то нам нужно инициаизировать обертки над хранилищем
+	if cfg.FileStoragePath == "" {
+		log.Info().Msg("Установлено хранилище в памяти. Сохранение на диск отключено")
+		return storage.AsOperator(m), func() {
+			log.Info().Msg("Хранилище сихронной записи получило сигнал о завершении, но файловая запись в текущей конфигурации сервера не используется. Ничего не записано")
+		}
+	}
+
+	// Оборачиваем файловым хранилищем, в случае, есл и
+	fs := storage.NewFileStorage(&m, cfg.FileStoragePath)
+	if cfg.Restore {
+		err := fs.Restore()
+		// в случае, если файла не существует, игнорируем эту проблему
+		if err != nil && err != storage.ErrorRestoreFileNotExist {
+			panic(err)
+		}
+		log.Info().Msgf("Значения метрик загружены из %v", fs.FileName)
+	}
+
+	if cfg.StoreIntervalSeconds == 0 {
+		// инициализируем сихнронную запись,
+		// при этом сохраняться в конце нам не понадобится
+		log.Info().Msgf("Установлено синхронное сохранение в %v в при записи", fs.FileName)
+		return storage.AsOperator(storage.NewSyncDump(&fs)), func() {
+			log.Info().Msg("Хранилище сихронной записи получило сигнал о завершении, но дополнительно сохранение не нужно")
+		}
+	}
+
+	// Запускаем интервальную запись и создаем токен отмены, при необходимости сюда можно будет добавить и группу ожидания
+	s := storage.NewIntervalDump(&fs, time.Duration(cfg.StoreIntervalSeconds)*time.Second)
+	ctx, cancel := context.WithCancel(context.Background())
+	go s.StartDumping(ctx)
+
+	log.Info().Msgf("Установлено сохранение с интервалом %v в %v в при записи", s.Interval, s.FileName)
+
+	return storage.AsOperator(s), func() {
+		// обертка сделана под группу ожидаения
+		cancel()
+	}
+
+	// TODO
+	//
+	// Пока что судя по всему эта функция эвакуирует моё хранилище из стека, мне кажется, но не мапы
+	// Как вариант сразу создавать FileStorage, и просто не оборачивать его если надо
 }
 
 func init() {
