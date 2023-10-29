9fdf40835fc5bbf068775b29e2485facc9885649
Dima Frolenko <mail@frolenkodima.ru>
2023-10-29 15:45:48 +0300

    fix: сервер УБрать лишние комменты

--- lib/graceful/serve.go
+++ lib/graceful/serve.go
@@ -14,43 +14,50 @@ import (
 // Serve запускает сервер с поддержкой нежного завершения. Сервер можно будет выключить через
 // SIGINT, SIGTERM, SIGQUIT
 func Serve(addr string, router http.Handler) {
-	// Запускаем сервер с поддержкой нежного выключения
-	// вдохноввлено примерами роутера chi
 	server := http.Server{Addr: addr, Handler: router}
 
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-
+	// запустим горутину, которая будет слушать сигналы от системы, и при получении
+	// начнет процедуру остановки сервера
 	go func() {
-		<-sig
+		<-RequestStop()
 		log.Debug().Msg("server wants to shut down")
-		// Shutdown signal with grace period of 30 seconds
+
 		shutdownCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 		defer cancel()
 
-		go func() {
-			<-shutdownCtx.Done()
-			if shutdownCtx.Err() == context.DeadlineExceeded {
-				log.Fatal().Msg("graceful shutdown timed out.. forcing exit.")
-				return
-			}
-		}()
-
-		// Trigger graceful shutdown
-		err := server.Shutdown(shutdownCtx)
-		if err != nil {
-			log.Fatal().Msg(err.Error())
-			panic(err)
-		}
-		log.Info().Msg("^-^ рутина остановки сервера завершилась")
+		ShutdownGracefullyContext(shutdownCtx, &server)
 	}()
 
 	log.Info().Msgf("^.^ Мяу, сервер запускается по адресу %v!", addr)
-
 	if err := server.ListenAndServe(); err != http.ErrServerClosed {
 		log.Error().Msgf("^0^ не могу запустить сервер: %v \n", err)
 	}
 	log.Error().Msg("Run() остановлен")
 
-	// не понимаю в таком случае вырубится ли тот потом что должен был сигналы ос
+	// если ошибка при запуске сервера, то горутина не не получит сигнал, но в общем её вырубит система как бэ
+}
+
+// RequestStop возвращает канал через который придёт сообщение, что операционная система запросила завершение работы
+func RequestStop() chan os.Signal {
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	return sig
+}
+
+func ShutdownGracefullyContext(ctx context.Context, serv *http.Server) {
+	go func() {
+		<-ctx.Done()
+		if ctx.Err() == context.DeadlineExceeded {
+			log.Fatal().Msg("Время вышло, сервер будет завершен принудительно")
+			return
+		}
+	}()
+
+	// Trigger graceful shutdown
+	err := serv.Shutdown(ctx)
+	if err != nil {
+		log.Fatal().Msg(err.Error())
+		panic(err)
+	}
+	log.Info().Msg("^-^ рутина остановки сервера завершилась")
 }
