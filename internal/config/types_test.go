9de77987dfd7713c7283b4a309f9df8395376f0e
Dima Frolenko <mail@frolenkodima.ru>
2023-10-30 04:38:31 +0300

    fix: config Назвать переменные в стиле Go

--- internal/config/types_test.go
+++ internal/config/types_test.go
@@ -9,17 +9,17 @@ import (
 )
 
 func Test_HideSecretFromConsole(t *testing.T) {
-	original_key := "my_key"
+	originalKey := "my_key"
 	key := config.Secret{}
-	key.Set(original_key)
-	output_to_console := fmt.Sprint(key)
-	assert.NotContains(t, output_to_console, original_key, "Вывод в консоль не должен содержать ключ исходный")
+	key.Set(originalKey)
+	consoleOutput := fmt.Sprint(key)
+	assert.NotContains(t, consoleOutput, originalKey, "Вывод в консоль не должен содержать ключ исходный")
 }
 
 func Test_HidePasswordFromConsole(t *testing.T) {
-	connstring := "host=localhost password=12342 dbname=123"
+	connString := "host=localhost password=12342 dbname=123"
 	cs := config.ConnectionString{}
-	cs.Set(connstring)
-	output_to_console := fmt.Sprint(cs)
-	assert.NotContains(t, output_to_console, "password", "Вывод в консоль не должен содержать пароль")
+	cs.Set(connString)
+	consoleOutput := fmt.Sprint(cs)
+	assert.NotContains(t, consoleOutput, "password", "Вывод в консоль не должен содержать пароль")
 }
