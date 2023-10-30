a635feb0467cab5b5837136302f26cb35030f001
Dima Frolenko <mail@frolenkodima.ru>
2023-10-30 05:52:34 +0300

    feat: сервис агента выделен в collector

--- cmd/agent/agent.go
+++ cmd/agent/agent.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"path"
-	"time"
 
 	"github.com/rs/zerolog/log"
-	"github.com/thefrol/kysh-kysh-meow/internal/collector/fetch"
+	"github.com/thefrol/kysh-kysh-meow/internal/collector"
 	"github.com/thefrol/kysh-kysh-meow/internal/collector/report"
 	"github.com/thefrol/kysh-kysh-meow/internal/compress"
 	"github.com/thefrol/kysh-kysh-meow/internal/config"
-	"github.com/thefrol/kysh-kysh-meow/lib/scheduler"
 )
 
 const updateRoute = "/updates"
@@ -31,44 +27,16 @@ func main() {
 	}
 
 	// Настроим отправку
+
+	// MENTOR я вообще не оч хорошо понимаю,
+	// нормально ли что это вообще какой-то левый пакет настривается тут?
+	// возможно все, вплоть до UpdateRoute должно уехать в конфиг, даже
+	// если такие переменные и не связаны с командной строкой. Зато красиво
+	// в одной месте можно оформить установку всех основных параметров
 	report.SetSigningKey(config.Key)
 	report.CompressLevel = compress.BestCompression
 	report.CompressMinLength = 100
 
 	// Запускаем работу
-	Serve(config)
-
-}
-
-// Endpoint формирует точку, куда агент будет посылать все запросы на основе своей текущей конфигурации
-func Endpoint(cfg config.Agent) string {
-	return fmt.Sprintf("%s%s", "http://", path.Join(cfg.Addr, updateRoute))
-}
-
-func Serve(config config.Agent) {
-	// Метрики собираются во временное хранилище s,
-	// где они хранятся в сыром виде и готовы превратиться
-	// в массив metrica.Metrica
-	var s fetch.Batcher
-
-	// запуск планировщика
-	c := scheduler.New()
-	//собираем данные раз в pollingInterval
-	c.AddJob(time.Duration(config.PollingInterval)*time.Second, func() {
-		//Обновляем данные в хранилище, тут же увеличиваем счетчик
-		log.Debug().Msg("Считывание метрик")
-		s = fetch.MemStats()
-	})
-	// отправляем данные раз в repostInterval
-	c.AddJob(time.Duration(config.ReportInterval)*time.Second, func() {
-		//отправляем на сервер метрики из хранилища s
-		err := report.Send(s.ToTransport(), Endpoint(config))
-		if err != nil {
-			log.Error().Msgf("Попытка отправить метрики завершилась с  ошибками: %v", err)
-			return
-		}
-	})
-
-	// Запускаем планировщик, и он занимает поток
-	c.Serve(200 * time.Millisecond)
+	collector.FetchAndReport(config, updateRoute)
 }
---
+++ internal/collector/collector.go
@@ -0,0 +1,46 @@
+package collector
+
+import (
+	"fmt"
+	"path"
+	"time"
+
+	"github.com/rs/zerolog/log"
+	"github.com/thefrol/kysh-kysh-meow/internal/collector/fetch"
+	"github.com/thefrol/kysh-kysh-meow/internal/collector/report"
+	"github.com/thefrol/kysh-kysh-meow/internal/config"
+	"github.com/thefrol/kysh-kysh-meow/lib/scheduler"
+)
+
+func FetchAndReport(config config.Agent, updateRoute string) {
+	// Метрики собираются во временное хранилище s,
+	// где они хранятся в сыром виде и готовы превратиться
+	// в массив metrica.Metrica
+	var s fetch.Batcher
+
+	// запуск планировщика
+	c := scheduler.New()
+	//собираем данные раз в pollingInterval
+	c.AddJob(time.Duration(config.PollingInterval)*time.Second, func() {
+		//Обновляем данные в хранилище, тут же увеличиваем счетчик
+		log.Debug().Msg("Считывание метрик")
+		s = fetch.MemStats()
+	})
+	// отправляем данные раз в repostInterval
+	c.AddJob(time.Duration(config.ReportInterval)*time.Second, func() {
+		//отправляем на сервер метрики из хранилища s
+		err := report.Send(s.ToTransport(), Endpoint(config.Addr, updateRoute))
+		if err != nil {
+			log.Error().Msgf("Попытка отправить метрики завершилась с  ошибками: %v", err)
+			return
+		}
+	})
+
+	// Запускаем планировщик, и он занимает поток
+	c.Serve(200 * time.Millisecond)
+}
+
+// Endpoint формирует точку, куда агент будет посылать все запросы на основе своей текущей конфигурации
+func Endpoint(addr, route string) string {
+	return fmt.Sprintf("%s%s", "http://", path.Join(addr, route))
+}
