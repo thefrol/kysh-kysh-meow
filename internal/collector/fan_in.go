aac62f25966d3c7e0041666fb9d54c7d8b52f00f
Dima Frolenko <mail@frolenkodima.ru>
2023-10-31 23:46:30 +0300

    fix: collector Исправить ожидающие группы FanIn

--- internal/collector/fan_in.go
+++ internal/collector/fan_in.go
@@ -2,19 +2,22 @@ package collector
 
 import (
 	"context"
+	"sync"
 
 	"github.com/thefrol/kysh-kysh-meow/internal/metrica"
 )
 
 func FanIn(ctx context.Context, chs ...<-chan metrica.Metrica) chan metrica.Metrica {
 	chMix := make(chan metrica.Metrica, len(chs)*generatorChannelSize)
+	fanWG := sync.WaitGroup{} // исплючительно внутренняя группа ожидания
 
 	// cоздаем воркера под каждый канал
 	for _, ch := range chs {
-		defer wg.Done()
-		wg.Add(1)
+
+		fanWG.Add(1)
 
 		go func(ch <-chan metrica.Metrica) {
+			defer fanWG.Done()
 
 			for v := range ch {
 				select {
@@ -24,14 +27,16 @@ func FanIn(ctx context.Context, chs ...<-chan metrica.Metrica) chan metrica.Metr
 					return
 				}
 			}
-
 		}(ch)
 	}
 
 	// горутина будет ожидать закрытия канала
+	wg.Add(1)
 	go func() {
-		wg.Wait()
+		fanWG.Wait()
 		close(chMix)
+		wg.Done() // todo потом эти все ожидания можно будет убрать, потому что по сути нам надо
+		// дождаться только остановки воркеров
 	}()
 
 	return chMix
