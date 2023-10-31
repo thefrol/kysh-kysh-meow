18c51b36043c85a3d1945ab634ac0facc2a5edfd
Dima Frolenko <mail@frolenkodima.ru>
2023-10-31 19:21:44 +0300

    fix: collector Очищать батч после отправки

--- internal/collector/workerpool.go
+++ internal/collector/workerpool.go
@@ -20,15 +20,16 @@ const MaxBatch = 40
 //
 // это на случай создания и закрытия множества каналов под каждую отправку
 
-func worker(inCh <-chan metrica.Metrica, url string) {
+func worker(inCh <-chan metrica.Metrica, url string, sema Semaphore) {
 	var batch []metrica.Metrica = make([]metrica.Metrica, 0, MaxBatch)
 	defer sendBatch(batch, url) // это конечно нужно тестировать какой именно батч он отправит ахах
 	// мне нужно все то, что осталось после закрытия канала входного
 
 	for v := range inCh {
 		batch = append(batch, v)
-		if len(batch) == MaxBatch {
+		if len(batch) >= MaxBatch {
 			sendBatch(batch, url)
+			batch = batch[:0]
 		}
 		// можно улучшить через default
 	}
