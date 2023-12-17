c0b174772b0553e615899754e08a84bb692d93dd
Dima Frolenko <mail@frolenkodima.ru>
2023-12-17 22:44:26 +0300

    feat: server Добавить новое апи

---
+++ internal/server/queryhandlers/api.go
@@ -0,0 +1,9 @@
+package queryhandlers
+
+import (
+	"github.com/thefrol/kysh-kysh-meow/internal/server/app/manager"
+)
+
+type API struct {
+	Registry manager.Registry
+}
---
+++ internal/server/queryhandlers/query.increment.counter.go
@@ -0,0 +1,30 @@
+package queryhandlers
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/thefrol/kysh-kysh-meow/internal/server/api"
+	"github.com/thefrol/kysh-kysh-meow/internal/server/domain"
+)
+
+func (a *API) IncrementCounter(w http.ResponseWriter, r *http.Request) {
+	var (
+		id = chi.URLParam(r, "id")
+	)
+
+	v, err := a.Registry.Counter(r.Context(), id)
+	if err != nil {
+		if errors.Is(err, domain.ErrorMetricNotFound) {
+			api.HTTPErrorWithLogging(w, http.StatusNotFound, "handler: GetCounter() не найдена метрика : %v", err)
+			return
+		}
+		api.HTTPErrorWithLogging(w, http.StatusBadRequest, "handler: GetCounter() handler : %v", err)
+		return
+	}
+
+	api.SetContentType(w, api.TypeTextPlain)
+	w.Write([]byte(strconv.FormatInt(v, 10)))
+}
