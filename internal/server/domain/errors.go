043d6359c191e4dfb5c1ccfd1d3c73cee114be00
Dima Frolenko <mail@frolenkodima.ru>
2023-12-17 22:16:21 +0300

    feat: domain добавить новую ошибку

--- internal/server/domain/errors.go
+++ internal/server/domain/errors.go
@@ -3,5 +3,6 @@ package domain
 import "errors"
 
 var (
-	ErrorBadID = errors.New("неправильный id")
+	ErrorBadID          = errors.New("неправильный id")
+	ErrorMetricNotFound = errors.New("метрика не найдена")
 )
