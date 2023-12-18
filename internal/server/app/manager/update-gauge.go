ef5094f2a27ebe58b60a6f6717a794951e6ab894
Dima Frolenko <mail@frolenkodima.ru>
2023-12-18 03:44:58 +0300

    fix: manager исправить сигнатуру юзкейса, обновляющего гауж

--- internal/server/app/manager/update-gauge.go
+++ internal/server/app/manager/update-gauge.go
@@ -7,10 +7,10 @@ import (
 	"github.com/thefrol/kysh-kysh-meow/internal/server/domain"
 )
 
-func (r Registry) UpdateGauge(ctx context.Context, id string, delta int64) (int64, error) {
+func (r Registry) UpdateGauge(ctx context.Context, id string, value float64) (float64, error) {
 	if err := IsValid(id); err != nil {
 		return 0, fmt.Errorf("%w: %v", domain.ErrorBadID, err)
 	}
 
-	return r.Counters.Increment(ctx, id, delta) // todo обернуть ошибку??
+	return r.Gauges.Update(ctx, id, value) // todo обернуть ошибку??
 }
