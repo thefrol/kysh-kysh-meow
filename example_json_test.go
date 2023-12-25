37d5d96083f6e67f77bbe2a50febfb0a415edc37
Dima Frolenko <mail@frolenkodima.ru>
2023-12-25 17:47:17 +0300

    doc: Добавить пример взаимодействия с сервером при помощи хелперов

---
+++ example_json_test.go
@@ -0,0 +1,38 @@
+package kyshkyshmeow_test
+
+import (
+	"log"
+	"math/rand"
+
+	kyshkyshmeow "github.com/thefrol/kysh-kysh-meow"
+)
+
+// Метрики можно отправлять пачкой, для этого их надо упаковать и
+// типы C и G, представляющие собой метрики типа: счетчик и величина.
+// и потом воспользоваться функцией BatchUpdate
+func Example_batch() {
+	// величина, которую я хочу передать
+	v := rand.Float64()
+
+	// Упаковываю
+	g := kyshkyshmeow.G{
+		ID:    "mygauge",
+		Value: v,
+	}
+
+	// и увеличим счетчик на 10
+	с := kyshkyshmeow.C{
+		ID:    "mycounter",
+		Delta: 10,
+	}
+
+	// мой сервер находится по адресу
+	addr := "localhost:8080"
+
+	// отправляем метрики
+	err := kyshkyshmeow.BatchUpdate(addr, g, с)
+	if err != nil {
+		log.Fatal("не могу обновить: %w", err)
+	}
+
+}
